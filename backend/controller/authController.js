b49b41aa6bea7559e9c01d6632fbf7d59717ce81
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2022-12-01 12:37:51 +0530

    Sending Authtoken to the user using bcrypt js and jsonwebtoken

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,9 @@
 const User = require('../models/User');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = "HarshWantsToBeWebDeveloper";
 
 
 module.exports.login = function (req, res) {
@@ -17,6 +21,10 @@ module.exports.signup =async function (req, res) {
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
     // Instead of getting error in console of unique user handle it
     let user = await User.findOne({ email: req.body.email })
     if (user) {
@@ -25,9 +33,15 @@ module.exports.signup =async function (req, res) {
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
         dueDate: req.body.dueDate
     });
-
-    res.json({ user })
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+    // implementing jsonwebtoken npm package which helps to validate authentic user
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json({ authToken })
 }
