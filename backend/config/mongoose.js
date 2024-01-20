476f4d048ed2895c85fffd14e1c2c2d90f1e2ab2
Harsh-Sisodia857 <harsh2012007@akgec.ac.in>
2024-01-20 22:54:31 +0530

    Added Mongodb atlas for storing User Data on Cloud

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://harshkumarsisodia99:yimjXg3xRfNGOEGD@cluster0.ks55g6u.mongodb.net/todoWebApp")
-
+mongoose.connect(process.env.DB_URI)
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console,"Error Connecting to Mongodb"))
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const port = 5000;
 const app = express();
-const db = require('./config/mongoose')
 var cors = require('cors') 
+require("dotenv").config()
+
+require('./config/mongoose')
 
 // To use req.body
 app.use(express.json())
@@ -12,7 +14,6 @@ app.use(cors())
 // use express router
 // require('./routes/index) is similar to require('./routes) in this case, it by default fetch routes
 app.use('/', require('./routes/index'))
-
 app.listen(port, function () {
     console.log(`Server is listening at port ${port}`);
 })
